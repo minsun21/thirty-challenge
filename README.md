2d22f8abd5c4db9a360fdf391d76d701c9a045a7
minsun21 <mspark0429@daum.net>
2020-10-26 23:09:47 +0900

    Progress 발동 조건 추가, enter key 추가

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 1. Percent
 - state인 PercentValue를 지우고 Percent로만 하면, 버튼을 클릭 하지 않고 입력하는대로 Progress가 변함
 - linear-gradient() ㅣ 그라데이션 함수
+- input key press
--- src/components/01Progress.js
+++ src/components/01Progress.js
@@ -4,20 +4,34 @@ function Progress() {
     const [Active, setActive] = useState('progress-done');
     const [PercentValue, setPercentValue] = useState();
     const [Percent, setPercent] = useState('');
+
     const progressHandler = () => {
-        setActive('progress-done-active');
-        setPercent(PercentValue);
+        if (PercentValue > 100) {
+            alert('100%이상은 안됨');
+        }
+        else {
+            let percent = PercentValue + '%';
+            setActive('progress-done-active');
+            setPercent(percent);
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            progressHandler();
+        }
     };
 
     const percentHandler = (e) => {
-        setPercentValue(e.target.value + '%');
+        setPercentValue(e.target.value);
     }
+
     return (
         <div className="progress--container">
             <div className="progress--container__progress">
                 <div className={Active} style={{ width: `${Percent}` }}>{Percent}</div>
             </div>
-            <input type="number" maxLength="3" onChange={percentHandler} placeholder="percent number" />
+            <input type="number" maxLength="3" onChange={percentHandler} onKeyPress={handleKeyPress} placeholder="percent number" />
             <button onClick={progressHandler}>Ok</button>
         </div>
     )
